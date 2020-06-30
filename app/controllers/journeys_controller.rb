f8fe29c56ce7b325555cca072c45f00969ab4339
Isaline Wethli <isalinewethli@hotmail.com>
2020-06-30 14:57:51 +0100

    added def show, new and create controller and view:show

--- app/controllers/journeys_controller.rb
+++ app/controllers/journeys_controller.rb
@@ -2,4 +2,18 @@ class JourneysController < ApplicationController
   def index
     @journeys = policy_scope(Journey)
   end
+
+  def show
+    @journey = Journey.find(params[:id])
+  end
+
+  def new
+    @journey = Journey.new
+  end
+
+  def create
+    @journey = Journey.new(params[:journey])
+    @journey.save
+    redirect_to action: "index"
+  end
 end
