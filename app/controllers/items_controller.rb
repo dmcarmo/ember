adf9c36a6fc96505035c3c213b8ee41d09581997
David Carmo <dmcarmo@gmail.com>
2020-07-01 21:40:42 +0100

    Fixed items table - renamed column back to type. Changed new & create from items to notes controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,22 +10,6 @@ class ItemsController < ApplicationController
     authorize @item
   end
 
-  def new
-    @journey = Journey.find(params[:journey_id])
-    @item = Item.new
-    authorize @item
-  end
-
-  def create
-    @item = Item.new(item_params)
-    @journey = Journey.find(params[:journey_id])
-    @item.journey = @journey
-    @item.item_type = "Notes"
-    @item.save
-    authorize @item
-    redirect_to main_journeys_path, notice: "Note saved"
-  end
-
   private
 
   def item_params
