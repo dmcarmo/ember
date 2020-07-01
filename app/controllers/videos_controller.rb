ba0e66ea03681026f1cf0cd8eb8ba8450f4b8c6d
David Carmo <dmcarmo@gmail.com>
2020-07-01 22:14:29 +0100

    Added routes, controllers and views for audio, video and picture.

---
+++ app/controllers/videos_controller.rb
@@ -0,0 +1,23 @@
+class VideosController < ItemsController
+
+  def new
+    @journey = Journey.find(params[:journey_id])
+    @video = Video.new
+    authorize @video
+  end
+
+  def create
+    @video = Video.new(video_params)
+    @journey = Journey.find(params[:journey_id])
+    @video.journey = @journey
+    @video.save
+    authorize @video
+    redirect_to main_journeys_path, notice: "Video saved"
+  end
+
+  private
+
+  def video_params
+    params.require(:video).permit(:name, :comment, :content)
+  end
+end
