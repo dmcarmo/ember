a44388c03d3d405ba3100803e6c4a39519a26360
David Carmo <dmcarmo@gmail.com>
2020-07-08 12:05:50 +0100

    Fixed devise config in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
   include Pundit
 
   # Pundit: white-list approach.
@@ -22,4 +23,12 @@ class ApplicationController < ActionController::Base
   def default_url_options
   { host: ENV["DOMAIN"] || "localhost:3000" }
   end
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
+
 end
